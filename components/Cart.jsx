f81ce4df79142f7c6323987a8329bb0082b397d4
pikacoder44 <pikaop125@gmail.com>
2025-09-14 21:23:31 +0500

    Stylized Cart comp

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { ShoppingCart } from 'lucide-react'
+import React from "react";
+import { ShoppingCart } from "lucide-react";
 const Cart = () => {
   return (
-    <div className='bg-zinc-600 p-4 rounded-lg flex flex-row gap-2'>
-      <ShoppingCart/>
-      <h2>Cart</h2>
-    </div>
-  )
-}
+    <div className="bg-zinc-800 p-4 rounded-lg flex flex-col items-center">
+      <div className="flex flex-row items-center text-blue-300 gap-2 mb-4">
+        <ShoppingCart />
+        <h2 className="text-2xl">Cart</h2>
+      </div>
 
-export default Cart
+      <li className="list-none">MacBookPro - $1000</li>
+      <h5 className="mt-2">Total: $2000000</h5>
+    </div>
+  );
+};
+
+export default Cart;
