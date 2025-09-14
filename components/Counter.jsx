ef41a96b64f32c28973a529ec1226b10a3ca7126
pikacoder44 <pikaop125@gmail.com>
2025-09-14 16:08:28 +0500

    gave buttons styling

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Counter = () => {
   return (
     <div>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <button className="bg-green-700 cursor-pointer hover:text-white hover:bg-green-500 p-2 m-2 rounded-lg text-black">
+        Increment
+      </button>
+      <button className="bg-red-700 cursor-pointer hover:text-white hover:bg-red-500 p-2 m-2 rounded-lg text-black">
+        Decrement
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
