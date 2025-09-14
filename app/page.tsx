9145ac062a85b22a9ab071447e645be23e5813b4
pikacoder44 <pikaop125@gmail.com>
2025-09-14 21:07:46 +0500

    GAve styling to item comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-
+import Item from "@/components/Item";
 export default function Home() {
- 
   return (
     <div className="font-sans flex flex-col items-center justify-center min-h-screen sm:p-20 bg-zinc-900">
-      
+      <Item name="MacBook Pro" price={100000} />
     </div>
   );
 }
--- components/Item.jsx
+++ components/Item.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Item = () => {
+const Item = (props) => {
   return (
-    <div>
-      
+    <div className="bg-zinc-700 p-6 rounded-md flex flex-col items-center justify-center gap-2">
+      <h4 className="text-xl">{props.name}</h4>
+      <p className="text-sm">Price : {props.price}$</p>
+      <button className="bg-blue-800 p-2 rounded-lg hover:bg-blue-700 hover:text-white text-black cursor-pointer">
+        Add to Cart
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
