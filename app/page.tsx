30854c6ebe7795615dcf49a945af464f4443d0b8
pikacoder44 <pikaop125@gmail.com>
2025-09-14 20:57:49 +0500

    Example 2- Ecommerce cart start

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 "use client";
-import { useContext } from "react";
-import { CounterContext } from "@/context/Counter";
-import Counter from "@/components/Counter";
+
 export default function Home() {
-  const counterState = useContext(CounterContext);
-  console.log("Content: ", counterState);
+ 
   return (
     <div className="font-sans flex flex-col items-center justify-center min-h-screen sm:p-20 bg-zinc-900">
-      <h1 className="text-2xl">Count is {counterState.count}</h1>
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
+      
     </div>
   );
 }
