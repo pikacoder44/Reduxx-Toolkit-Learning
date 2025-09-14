1a5031514f0a49d875c4d0e356d3d930038a5be4
pikacoder44 <pikaop125@gmail.com>
2025-09-14 20:56:13 +0500

    Example 1- Counter button - done

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   const counterState = useContext(CounterContext);
   console.log("Content: ", counterState);
   return (
-    <div className="font-sans flex flex-col items-center justify-center min-h-screen sm:p-20">
-      <h1>Count is {counterState.count}</h1>
+    <div className="font-sans flex flex-col items-center justify-center min-h-screen sm:p-20 bg-zinc-900">
+      <h1 className="text-2xl">Count is {counterState.count}</h1>
       <Counter />
       <Counter />
       <Counter />
