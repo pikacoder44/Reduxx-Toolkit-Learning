f05fcc954da9ad355e5b395864b97fe8536b0fd1
pikakodar44 <pikaop125@gmail.com>
2025-09-15 20:23:35 +0500

    Starting Redux Toolkit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { CartProvider } from "@/context/Cart";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <CartProvider>{children}</CartProvider>
+        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
-"use client";
-import Item from "@/components/Item";
-import Cart from "@/components/Cart";
+
 export default function Home() {
   return (
     <div className="font-sans gap-4 flex flex-col items-center justify-center min-h-screen sm:p-20 bg-zinc-900">
-      <div className="grid grid-cols-3 gap-4">
-        <Item name="MacBook Pro" price={100000} />
-        <Item name="Pendrive" price={4000} />
-        <Item name="Iphone XR" price={40000} />
-      </div>
-      <Cart />
+    
     </div>
   );
 }
