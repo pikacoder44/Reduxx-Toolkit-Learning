9f8b554d7c215b32fe29a32dce824ca9aed13da5
pikakodar44 <pikaop125@gmail.com>
2025-09-16 20:23:48 +0500

    Cleared hs

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="font-sans gap-4 flex flex-col items-center justify-center min-h-screen sm:p-20 bg-zinc-900">
-    
+    Redux Toolkit
     </div>
   );
 }
