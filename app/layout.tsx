fcf5ba9dd3e1b3b1e72fc1578c9de36b3627ae27
pikakodar44 <pikaop125@gmail.com>
2025-09-19 05:59:15 +0500

    Added store and provider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Provider } from "react-redux";
-
+import { store } from "/redux/store";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>{children}</Provider>
+        <Provider store={store}>{children}</Provider>
       </body>
     </html>
   );
