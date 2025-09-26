82015940bcaae6ad54dd04e1aff2303a0447ec0a
pikacoder44 <pikaop125@gmail.com>
2025-09-26 03:28:47 -0700

    Exported addTodo,removeTodo,Reducers

--- features/todo/todoSlice.js
+++ features/todo/todoSlice.js
@@ -21,8 +21,6 @@ export const todoSlice = createSlice({
   },
 });
 
-export const {addTodo,removeTodo} = todoSlice.actions;
+export const { addTodo, removeTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
-
-
