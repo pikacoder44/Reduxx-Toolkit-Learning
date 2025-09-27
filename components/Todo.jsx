8b6766501f555c8784e621a9e27236067cf49787
pikacoder44 <pikaop125@gmail.com>
2025-09-26 18:12:57 -0700

    Configured AddTodo comp to dispatch changed

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
 const Todo = () => {
-  return (
-    <div>Todo</div>
-  )
-}
+  return <div>Todo</div>;
+};
 
-export default Todo
+export default Todo;
