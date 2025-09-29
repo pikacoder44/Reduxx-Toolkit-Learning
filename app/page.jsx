9a9b32b28891490ea351c78f8d32e4bf08497d0e
pikacoder44 <pikaop125@gmail.com>
2025-09-29 19:27:16 +0500

    Starting Assignment Now

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Todo from "@/components/Todo"
 export default function Home() {
   return (
     <div className="font-sans gap-4 flex flex-col items-center justify-center min-h-screen sm:p-20 bg-zinc-900">
-      <h1>Redux Toolkit</h1>
+      <h1>Redux Toolkit Learning</h1>
       <AddTodo />
       <Todo/>
     </div>
